84dc3226a6b1bf3440d4cc93f040d3d515a63604
Aashish Thapa <aashishthapa889@gmail.com>
2024-08-08 16:52:55 +0545

    navbar addedd

---
+++ lib/features/authentication/presentation/pages/profile_page/profile_page_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ProfilePageScreen extends StatefulWidget {
+  const ProfilePageScreen({super.key});
+
+  @override
+  State<ProfilePageScreen> createState() => _ProfilePageScreenState();
+}
+
+class _ProfilePageScreenState extends State<ProfilePageScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/features/authentication/presentation/pages/reels_page/reels_page_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ReelsPageScreen extends StatefulWidget {
+  const ReelsPageScreen({super.key});
+
+  @override
+  State<ReelsPageScreen> createState() => _ReelsPageScreenState();
+}
+
+class _ReelsPageScreenState extends State<ReelsPageScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
